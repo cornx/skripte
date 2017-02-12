88ca174792faa9cc66c1f698a9e0749a884c45f4
cornx <exit>
2017-02-12 16:54:37 +0100

    baffle dodan

---
+++ baffle/js/encrypt.js
@@ -0,0 +1,33 @@
+var p = "#para",
+    encrypt = document.getElementById("para"),
+    x = "▒";
+
+let a = baffle(p, {
+    characters: '~#^*|/█▓▒░█▓▒░█▓▒░',
+    speed: 75
+})
+
+a.once(); // početno šifriranje
+
+
+
+// dešifriranje i kasnije šifriranje
+encrypt.addEventListener("click", function() {
+    if (event.shiftKey) {
+        if (encrypt.innerHTML.indexOf(x) > -1) {
+            a.reveal(1000);
+            oznaci();
+        } else {
+            a.once();
+        }
+    }
+});
+
+
+
+function oznaci() {
+    $('#para').each(function() {
+        var skrivenaPoruka = $(this).text().replace(/SKRIVENA PORUKA /g, "<span class='poruka'>SKRIVENA PORUKA </span>");
+        $(this).html(skrivenaPoruka);
+    });
+}
