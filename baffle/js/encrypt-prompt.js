b63b9705005b3f885fc67a2b8a24723425fd40bd
cornx <exit>
2017-02-12 19:11:09 +0100

    baffle prompt

--- baffle/js/encrypt-prompt.js
+++ baffle/js/encrypt-prompt.js
@@ -10,15 +10,17 @@ let a = baffle(p, {
 a.once(); // početno šifriranje
 
 // var unos = null;
-
+var unos = null;
 function sifra() {
-    a.once();
-    var unos = prompt("Upiši šifru kako bi vidio sadržaj");
+    unos = prompt("Upiši šifru kako bi vidio sadržaj");
     if (unos === null || unos === undefined || unos === "") {
         a.once();
 
     } else if (unos === "šifra") {
         a.reveal(1000);
+    }else {
+        a.once();
     }
 }
-sifra();
+
+setTimeout(function(){sifra();}, 500);
